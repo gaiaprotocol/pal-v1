74b32ebcaf37901a83ad050961f63a9b03e52431
Sim Young Jae <hanul@hanul.me>
2023-09-14 13:07:41 +0900

    Test price fomula

---
+++ test/Pal.test.cjs
@@ -0,0 +1,57 @@
+const { ethers } = require("hardhat");
+const { expect } = require("chai");
+const random = require("random-bigint");
+
+let Pal;
+
+function getPrice(supply, amount) {
+  const sum1 = supply == 0n
+    ? 0n
+    : (supply - 1n) * (supply) * (2n * (supply - 1n) + 1n) / 6n;
+  const sum2 = supply == 0n && amount == 1n
+    ? 0n
+    : (supply - 1n + amount) * (supply + amount) *
+      (2n * (supply - 1n + amount) + 1n) / 6n;
+  const summation = sum2 - sum1;
+  return summation * ethers.parseEther("1") / 16000n;
+}
+
+describe("Pal", () => {
+  beforeEach(async () => {
+    Pal = await (await ethers.getContractFactory("Pal")).deploy();
+  });
+
+  it("price test", async () => {
+    const supply = 0n;
+    const amount = 1n;
+    const price = getPrice(supply + 1n, amount);
+    const palPrice = await Pal.getPrice(
+      supply * ethers.parseEther("1"),
+      amount * ethers.parseEther("1"),
+    );
+    /*console.log(
+      `supply: ${supply}, amount: ${amount}, price: ${price}, palPrice: ${palPrice}`,
+    );*/
+    expect(palPrice).to.equal(price);
+
+    for (let i = 0; i < 100; i++) {
+      const supply = random(32);
+      const amount = random(32);
+      const price = getPrice(supply + 1n, amount);
+      const palPrice = await Pal.getPrice(
+        supply * ethers.parseEther("1"),
+        amount * ethers.parseEther("1"),
+      );
+      /*console.log(
+        `supply: ${supply}, amount: ${amount}, price: ${price}, palPrice: ${palPrice}`,
+      );*/
+      expect(palPrice).to.equal(price);
+    }
+
+    const palPrice2 = await Pal.getPrice(
+      ethers.parseEther("0.0000001"),
+      ethers.parseEther("0.0000001"),
+    );
+    console.log(ethers.formatEther(palPrice2));
+  });
+});
