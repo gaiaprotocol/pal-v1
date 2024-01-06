92cf5eee012e1f6e6702f2b960aa158d86c1d704
yjgaia <hanul@hanul.me>
2024-01-06 23:22:50 +0900

    dev

--- database/create-function/parse_contract_event.sql
+++ database/create-function/parse_contract_event.sql
@@ -2,7 +2,18 @@ CREATE OR REPLACE FUNCTION "public"."parse_contract_event"() RETURNS "trigger"
     LANGUAGE "plpgsql" SECURITY DEFINER
     AS $$
 BEGIN
-    --TODO: implement
+    IF new.event_name = 'UserTokenCreated' THEN
+        -- add token info
+        insert into tokens (
+            chain, token_address, owner, name, symbol
+        ) values (
+            new.chain, new.args[2], new.args[1], new.args[3], new.args[4]
+        );
+    ELSIF new.event_name = 'Trade' THEN
+        -- update token info
+        -- update token holder info
+        -- update wallet's total key balance
+    END IF;
     RETURN NULL;
 end;$$;
 
