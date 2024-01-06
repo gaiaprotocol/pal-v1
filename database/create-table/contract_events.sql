3ffcdab00b8155c24598eae7662581c8e8fcafcf
yjgaia <hanul@hanul.me>
2024-01-06 22:38:06 +0900

    Add contract_events table

---
+++ database/create-table/contract_events.sql
@@ -0,0 +1,21 @@
+CREATE TABLE IF NOT EXISTS "public"."contract_events" (
+    "chain" "text" NOT NULL,
+    "block_number" bigint NOT NULL,
+    "log_index" bigint NOT NULL,
+    "event_name" "text" NOT NULL,
+    "args" "text" DEFAULT '[]'::"text" NOT NULL,
+    "wallet_address" "text" NOT NULL,
+    "token_address" "text" NOT NULL,
+    "created_at" timestamp with time zone DEFAULT "now"() NOT NULL
+);
+
+ALTER TABLE "public"."contract_events" OWNER TO "postgres";
+
+ALTER TABLE ONLY "public"."contract_events"
+    ADD CONSTRAINT "contract_events_pkey" PRIMARY KEY ("chain", "block_number", "log_index");
+
+ALTER TABLE "public"."contract_events" ENABLE ROW LEVEL SECURITY;
+
+GRANT ALL ON TABLE "public"."contract_events" TO "anon";
+GRANT ALL ON TABLE "public"."contract_events" TO "authenticated";
+GRANT ALL ON TABLE "public"."contract_events" TO "service_role";
